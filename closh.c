a32a38f597e7d378da9642b83dce9e606eadb393
Rock-it-science <wilmc17@gmail.com>
2020-02-05 15:38:05 -0800

    Fix whitespace issue sequential waitpid typo

--- closh.c
+++ closh.c
@@ -16,119 +16,117 @@
 #define FALSE 0
 
 void kill_process(int sig) {//kill processes and its children
-    kill(0, SIGTERM);
+	kill(0, SIGTERM);
 }
 
 // tokenize the command string into arguments - do not modify
 void readCmdTokens(char* cmd, char** cmdTokens) {
-    cmd[strlen(cmd) - 1] = '\0'; // drop trailing newline
-    int i = 0;
-    cmdTokens[i] = strtok(cmd, " "); // tokenize on spaces
-    while (cmdTokens[i++] && i < sizeof(cmdTokens)) {
-        cmdTokens[i] = strtok(NULL, " ");
-    }
+	cmd[strlen(cmd) - 1] = '\0'; // drop trailing newline
+	int i = 0;
+	cmdTokens[i] = strtok(cmd, " "); // tokenize on spaces
+	while (cmdTokens[i++] && i < sizeof(cmdTokens)) {
+		cmdTokens[i] = strtok(NULL, " ");
+	}
 }
 
 // read one character of input, then discard up to the newline - do not modify
 char readChar() {
-    char c = getchar();
-    while (getchar() != '\n');
-    return c;
+	char c = getchar();
+	while (getchar() != '\n');
+	return c;
 }
 
 //Function to tell user that command timed out
 void timeout_handler(int signum){
 	printf("Command timed out\n");
-	exit(1);
+	kill(getpid(), 0);
 }
 
 // main method - program entry point
 int main() {
-    char cmd[81]; // array of chars (a string)
-    char* cmdTokens[20]; // array of strings
-    int count; // number of times to execute command
-    int parallel; // whether to run in parallel or sequentially
-    int timeout; // max seconds to run set of commands (parallel) or each command (sequentially)
-    
-    while (TRUE) { // main shell input loop
-        
-        // begin parsing code - do not modify
-        printf("closh> ");
-        fgets(cmd, sizeof(cmd), stdin);
-        if (cmd[0] == '\n') continue;
-        readCmdTokens(cmd, cmdTokens);
-        do {
-            printf("  count> ");
-            count = readChar() - '0';
-        } while (count <= 0 || count > 9);
-        
-        printf("  [p]arallel or [s]equential> ");
-        parallel = (readChar() == 'p') ? TRUE : FALSE;
-        do {
-            printf("  timeout> ");
-            timeout = readChar() - '0';
-        } while (timeout < 0 || timeout > 9);
-        // end parsing code
-        
-        
-        ////////////////////////////////////////////////////////
-        //                                                    //
-        // TODO: use cmdTokens, count, parallel, and timeout  //
-        // to implement the rest of closh                     //
-        //                                                    //
-        // /////////////////////////////////////////////////////
+	char cmd[81]; // array of chars (a string)
+	char* cmdTokens[20]; // array of strings
+	int count; // number of times to execute command
+	int parallel; // whether to run in parallel or sequentially
+	int timeout; // max seconds to run set of commands (parallel) or each command (sequentially)
+	
+	while (TRUE) { // main shell input loop
+		
+		// begin parsing code - do not modify
+		printf("closh> ");
+		fgets(cmd, sizeof(cmd), stdin);
+		if (cmd[0] == '\n') continue;
+		readCmdTokens(cmd, cmdTokens);
+		do {
+			printf("  count> ");
+			count = readChar() - '0';
+		} while (count <= 0 || count > 9);
+		
+		printf("  [p]arallel or [s]equential> ");
+		parallel = (readChar() == 'p') ? TRUE : FALSE;
+		do {
+			printf("  timeout> ");
+			timeout = readChar() - '0';
+		} while (timeout < 0 || timeout > 9);
+		// end parsing code
+		
+		
+		////////////////////////////////////////////////////////
+		//													//
+		// TODO: use cmdTokens, count, parallel, and timeout  //
+		// to implement the rest of closh					 //
+		//													//
+		// /////////////////////////////////////////////////////
 		int status, child_pid;
 		int child_pids[count];
 		
 		if(parallel){//Run in parallel
 			//Run program 'count' times, and set a timeout for the duration of the execution as 'timeout' seconds
-            signal(SIGALRM, timeout_handler);
-			alarm(timeout); //Set the timeout alarm
+			signal(SIGALRM, timeout_handler);
 			for (int i = 0; i < count; i++) {
 				
 				child_pids[i] = fork(); //Fork process
 				
-                if (child_pids[i] == 0) {//Child process
+				if (child_pids[i] == 0) {//Child process
 					//printf("pid: %d\n", getpid()); //print process id
-                    execvp(cmdTokens[0], cmdTokens);
-                    printf("Can't execute %s\n", cmdTokens[0]); // only reached if running the program failed
-                    // doesn't return unless the calling failed
-                    exit(1);
-                }
-            }
-			//Wait for all child processes to finish or timeout (whichever is first)
-			sleep(0.1);//Give program a chance to start
-			for(int j=0; j<count; j++){
-				waitpid(child_pids[j], 0, 0);
+					execvp(cmdTokens[0], cmdTokens);
+					printf("Can't execute %s\n", cmdTokens[0]); // only reached if running the program failed
+					// doesn't return unless the calling failed
+					exit(1);
+				}
 			}
-			alarm(0); //Disable timeout alarm
+			//Wait for all child processes to finish or timeout (whichever is first)
+			for(int j=0; j<count; j++){
+				alarm(timeout); //Set the timeout alarm for this process
+				waitpid(child_pids[j], 0, 0);
+				alarm(0); //Disable timeout alarm
+			}
 			//printf("All Processses executed successfully\n");
 		}
 		
 		else{//Run sequentially
 			//Run program 'count' times, waiting for timeout each time
-            for (int i = 0; i < count; i++) {
+			for (int i = 0; i < count; i++) {
 				
 				child_pid = fork(); //Fork process
 				
-                if (child_pids == 0) {//Child process
+				if (child_pid == 0) {//Child process
 					//printf("\npid: %d\n", getpid()); //print process id
-                    execvp(cmdTokens[0], cmdTokens);
-                    printf("Can't execute %s\n", cmdTokens[0]); // only reached if running the program failed
-                    // doesn't return unless the calling failed
-                    exit(1);
-                }
+					execvp(cmdTokens[0], cmdTokens);
+					printf("Can't execute %s\n", cmdTokens[0]); // only reached if running the program failed
+					// doesn't return unless the calling failed
+					exit(1);
+				}
 				
 				else{//Parent process
 					//Wait for child process to finish or timeout (whichever is first)
 					signal(SIGALRM, timeout_handler);
 					alarm(timeout);
-					sleep(0.1);//Give program a chance to start
 					waitpid(child_pid, 0, 0);
-					alarm(0);
+					alarm(0); // Cancel alarm
 					//printf("Processs successful\n");
 				}
-            }
-		}      
-    }
+			}
+		}	  
+	}
 }
